42f49003d79f32bb9be876f9fd89a467163b1bdb
dikshant rajput <57234452+dikshantrajput@users.noreply.github.com>
2021-10-22 23:01:13 +0530

    Create additionInLL.cpp

---
+++ CPP/LinkedList/additionInLL.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+class Node{
+  public:
+    int data;
+    Node* next;
+    Node(int data){
+      this->data = data;
+      this->next = NULL;
+    }
+};
+
+void printList(Node* head){
+  while(head!=NULL){
+    cout<<head->data<<" ";
+    head = head->next;
+  }
+}
+
+void addNodeAtEnd(Node* head, int x){
+   while(head->next!=NULL){
+      head = head->next;
+    }
+  
+    head->next = new Node(x);
+  
+}
+
+Node* addNodeAtBeginning(Node* head, int x){
+    Node* new_head = new Node(x);
+    
+    if(!head){
+      retrun new_head;
+    }
+
+    new_head->next = head;
+
+    return new_head;
+}
+
+int main(){
+
+  Node* head = new Node(20);
+
+  head->next = new Node(30);
+
+  head->next->next = new Node(40);
+
+  printList(head);
+  
+  cout<<endl;
+  
+  addNodeAtEnd(head,50);
+  
+  Node* new_head = addNodeAtBeginning(head,10);
+  
+  cout<<"After addition"<<printList(new_head)<<endl;
+
+  return 0;
+}
