38c774d87e0ba31e0da0ba384f9136dc61579824
Pratyush Raj <42607563+pratyushraj7@users.noreply.github.com>
2021-10-28 10:19:18 +0530

    Update Single Element in a Sorted Array.cpp

--- CPP/Array Practice/Single Element in a Sorted Array.cpp
+++ CPP/Array Practice/Single Element in a Sorted Array.cpp
@@ -1,20 +1,32 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) 
-    {
-        if(nums.empty())
-            return 0;
-            
-        unordered_map<int, int> map;
-        
-        for(int num : nums)
-            map[num]++;
-        
-        for(auto s : map)
-        {
-            if(s.second == 1)
-                return s.first;
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// A Linear Search based function to find
+// the element that appears only once
+void search(int arr[], int n)
+{
+    int ans = -1;
+    for (int i = 0; i < n; i += 2) {
+        if (arr[i] != arr[i + 1]) {
+            ans = arr[i];
+            break;
         }
-        return 0;
     }
-};
+   
+    if (arr[n - 2] != arr[n - 1])
+            ans = arr[n-1];
+   
+    // ans = -1 if no such element is present.
+    cout << "The required element is " << ans << "\n";
+}
+ 
+// Driver code
+int main()
+{
+    int arr[] = { 1, 1, 2, 4, 4, 5, 5, 6, 6 };
+    int len = sizeof(arr) / sizeof(arr[0]);
+ 
+    search(arr, len);
+ 
+    return 0;
+}
