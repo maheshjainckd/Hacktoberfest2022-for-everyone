218d273d3e30054382d3be28e22639c95fae6ea3
Vedika Bhutani <vedikabhutani5@gmail.com>
2021-10-30 16:40:29 +0530

    Create Replace_with_sum_of_greater_node_BST.cpp

---
+++ CPP/Replace_with_sum_of_greater_node_BST.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+void printLevelATNewLine(BinaryTreeNode<int> *root) {
+    if (root == NULL) {
+        return;
+    }
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *first = q.front();
+        q.pop();
+        if (first == NULL) {
+            if (q.empty()) {
+                break;
+            }
+            cout << endl;
+            q.push(NULL);
+            continue;
+        }
+        cout << first->data << " ";
+        if (first->left != NULL) {
+            q.push(first->left);
+        }
+        if (first->right != NULL) {
+            q.push(first->right);
+        }
+    }
+}
+void helper(BinaryTreeNode<int> *root,int &sum)
+{
+	if(root==NULL)
+        return;
+    helper(root->right,sum);
+    sum+=root->data;
+    root->data=sum;
+    helper(root->left,sum);    
+}
+void replaceWithLargerNodesSum(BinaryTreeNode<int> *root) {
+    if(root==NULL)
+        return;
+    int sum=0;
+    helper(root,sum);
+    return;
+}
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    replaceWithLargerNodesSum(root);
+    printLevelATNewLine(root);
+    delete root;
+}
