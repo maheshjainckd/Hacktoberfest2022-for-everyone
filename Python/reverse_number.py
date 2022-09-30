0d8c545d554b8a725b0a3dcd7c2e321eaf52c15b
Shashank Singh <ssingh0271@gmail.com>
2022-09-30 20:49:21 +0530

    added python program to reverse an integer using while loop

---
+++ Python/reverse_number.py
@@ -0,0 +1,11 @@
+#python program to reverse a number using while loop
+
+num = 1234 #number to be reversed
+reversed_num = 0
+
+while num != 0:
+    digit = num % 10
+    reversed_num = reversed_num * 10 + digit
+    num //= 10
+
+print("Reversed Number: " + str(reversed_num))
