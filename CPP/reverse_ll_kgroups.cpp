6d538b05288b6933a8ad97e69d248e72c8942950
khetanishq <53037714+TanishqKhetan@users.noreply.github.com>
2022-10-01 17:09:36 +0530

    Update reverse_ll_kgroups.cpp

--- CPP/reverse_ll_kgroups.cpp
+++ CPP/reverse_ll_kgroups.cpp
@@ -1,31 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@TanishqKhetan 
-loveBabbar
-/
-CodeHelp-DSA-Busted-Series
-Public
-Code
-Issues
-28
-Pull requests
-129
-Actions
-Projects
-Security
-Insights
-CodeHelp-DSA-Busted-Series/Lecture046 Linked List Day3/reverseInKGroup.cpp
-
-Love Babbar Adding codes for previous Lectures
-Latest commit f26a642 on 16 Feb
- History
- 0 contributors
-46 lines (38 sloc)  947 Bytes
 
 /****************************************************************
     Following is the Linked List node structure
@@ -71,18 +43,4 @@ Node* kReverse(Node* head, int k) {
     //step3: return head of reversed list
     return prev;
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-CodeHelp-DSA-Busted-Series/reverseInKGroup.cpp at main · loveBabbar/CodeHelp-DSA-Busted-Series
+
