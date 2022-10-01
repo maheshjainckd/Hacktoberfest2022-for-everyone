960711b08f95109bff9c4cc9f1cbb8d9c33ffd2f
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-10-01 13:33:51 +0530

    Added a Leap year checker program and updated my name in contributers.html

---
+++ C/Leap-Year-checker.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
+
+   if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else {
+      printf("%d is not a leap year.", year);
+   }
+   return 0;
+}
