17fe5779fa6e8ad09ad1a346bdeefdcbbfb16cab
sainath goud <saisainath777@gmail.com>
2022-10-01 11:36:05 +0530

    commit-message

---
+++ JavaScript/timer.js
@@ -0,0 +1,23 @@
+!DOCTYPE <html>
+
+<html>
+
+<body>
+
+<h2>JavaScript Timing Sample</h2>
+
+<p>Click on "Try it". Wait 5 seconds, and the page will alert "Hello How are you!!".</p>
+
+<button onclick="setTimeout(myFunction, 5000);">Try it</button>
+
+<script>
+
+function myFunction() {
+  alert('Hello How are you!!')
+}
+
+</script>
+
+</body>
+
+</html>
