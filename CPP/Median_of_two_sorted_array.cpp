6a4183039d027510766527ee11a4357b0c989df8
Naman21-web <82868154+Naman21-web@users.noreply.github.com>
2022-10-01 18:58:58 +0530

    Create Median_of_two_sorted_array.cpp

---
+++ CPP/Median_of_two_sorted_array.cpp
@@ -0,0 +1,53 @@
+// C++ program for the above approach
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int Solution(int arr[], int n)
+{
+  
+    // If length of array is even
+     if (n % 2 == 0)
+     {
+       int z = n / 2;
+       int e = arr[z];
+       int q = arr[z - 1];
+       int ans = (e + q) / 2;
+       return ans;
+     }
+    
+     // If length if array is odd
+    else
+     {
+       int z = round(n / 2);
+       return arr[z];
+     }
+}
+
+int main() {
+    
+        int arr1[] = { -5, 3, 6, 12, 15 };
+        int arr2[] = { -12, -10, -6, -3, 4, 10 };
+ 
+        int i =  sizeof(arr1) / sizeof(arr1[0]);
+        int j =  sizeof(arr2) / sizeof(arr2[0]);
+ 
+        int arr3[i+j];
+        int l =  i+j;
+        // Merge two array into one array
+        for(int k=0;k<i;k++)
+        {
+            arr3[k]=arr1[k];
+        }
+          
+        int a=0; 
+        for(int k=i;k<l;k++)
+        {
+            arr3[k]=arr2[a++];
+        }
+ 
+        // Sort the merged array
+        sort(arr3,arr3+l);
+ 
+        // calling the method
+        cout<<"Median = " << Solution(arr3, l);
+}
