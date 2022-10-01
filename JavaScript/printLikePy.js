78901fd7097d2c17fcc6e64c469a25baadffbe56
Yash Raj <87474641+programmer-offbeat@users.noreply.github.com>
2022-10-01 12:27:53 +0530

    Print like python
    assigned console.log to a print function

---
+++ JavaScript/printLikePy.js
@@ -0,0 +1,12 @@
+print("Hello world !");
+
+function print(textToPrint){
+    console.log(textToPrint());
+}
+
+/**********************************************
+ * @INFO
+ * Author = programmer-offbeat  
+ * @INFO
+ * Discord = Astrexx.jar#4035
+ *********************************************/
