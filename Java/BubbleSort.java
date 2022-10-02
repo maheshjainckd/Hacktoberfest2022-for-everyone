6e33190c9bd862aa42c72282cf758692d111c7ca
Shankar Ligade <78978131+shankarligade77@users.noreply.github.com>
2022-10-02 11:10:48 +0530

    Create BubbleSort.java

---
+++ Java/BubbleSort.java
@@ -0,0 +1,18 @@
+public class Bubblesort {
+    public static void main(String[] args) {
+        int[] arr={1,5,4,3,6,2,8,5,9};
+        int n= arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
