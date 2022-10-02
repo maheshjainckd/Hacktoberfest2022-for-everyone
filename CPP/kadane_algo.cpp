b7fe163158e87f8a00f5706893ab8352eed7faab
gorav khetan <91839255+Tanishq-khetan@users.noreply.github.com>
2022-10-02 13:10:41 +0530

    Update kadane_algo.cpp

--- CPP/kadane_algo.cpp
+++ CPP/kadane_algo.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 int maxSubArraySum(int a[], int size)
 {
-	int max_so_far = INT_MIN, max_ending_here = 0;
+	int maxi=arr[0];
+	int sum=0;
 
 	for (int i = 0; i < size; i++)
 	{
-		max_ending_here = max_ending_here + a[i];
-		if (max_so_far < max_ending_here)
-			max_so_far = max_ending_here;
-
-		if (max_ending_here < 0)
-			max_ending_here = 0;
+		sum=sum+arr[i];
+		maxi=max(maxi,sum);
+		if(sum<0)
+		{
+			sum=0;
+		}
+			
 	}
-	return max_so_far;
+	return maxi;
 }
 
 /*Driver program to test maxSubArraySum*/
