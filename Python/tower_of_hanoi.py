7046c55a6cf3b519a027cb3c8ee2e9566224a633
PRai <83975944+Priyanshi-Rai@users.noreply.github.com>
2022-10-05 08:16:14 +0530

    Added the guidelines of the game Tower of Hanoi

--- Python/tower_of_hanoi.py
+++ Python/tower_of_hanoi.py
@@ -1,6 +1,17 @@
 #Author's name : Priyanshi Rai
 #Modification date : 5-october-2022
 
+#---------------------------------------------------GUIDELINES---------------------------------------------------------------------------------------
+
+#1.) Create a tower_of_hanoi recursive function and pass two arguments: the number of disks n and the name of the rods such as source, aux, and target.
+#2.) We can define the base case when the number of disks is 1. In this case, simply move the one disk from the source to target and return.
+#3.) Now, move remaining n-1 disks from source to auxiliary using the target as the auxiliary.
+#4.) Then, the remaining 1 disk move on the source to target.
+#5.) Move the n-1 disks on the auxiliary to the target using the source as the auxiliary.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 ##Program to solve the Tower of Hanoi problem using python
 
 # Creating a recursive function  
