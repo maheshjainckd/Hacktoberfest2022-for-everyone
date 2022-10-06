c89e45670456480d2574fd5ed5e47cd7ea128221
Ramanujam Gond <ramanujamgond@gmail.com>
2022-10-06 15:21:03 +0530

    Added two methods in javascript directory to reverse a string and to find the perimeter of rectangle

---
+++ JavaScript/Array/Problems/perimeterOfRectange.js
@@ -0,0 +1,7 @@
+// Function to return the perimeter of the rectangle when length and breadth are passed as arguments
+
+function perimeterRectangle(length, breadth) {
+    return 2 * (length + breadth);
+}
+
+console.log('Perimeter of the rectangle is ' + perimeterRectangle(2, 18) + ' cm');
