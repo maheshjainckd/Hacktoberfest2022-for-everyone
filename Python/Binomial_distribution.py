c3dbd87ca4cafcdbe15507f675bd94f04929918a
HamidAli110 <110588157+HamidAli110@users.noreply.github.com>
2022-10-06 15:37:36 +0500

    Update Binomial_distribution.py

--- Python/Binomial_distribution.py
+++ Python/Binomial_distribution.py
@@ -1,14 +1,5 @@
-# find N number of samples in a binomial distribution of X number of coin flips and Y probability of heads with optional seed value (S)
-import numpy as np
-import random
-from scipy.stats import binom
-seed=int(input("Enter seed value: "))
-np.random.seed(seed)
-
-X = int(input("Enter number of coin flips: "))
-Y = int(input("Enter probability of heads: "))
-Distribution = np.random.binomial(X,Y)
-print(Distribution)
-
-print("Mean of the given Binomial Distribution is {:0.2f}".format(np.mean(Distribution)))
-print("Variance of the given Binomial Distribution is {:0.2f}".format(np.var(Distribution)))
+# typecating is a way to convert one data type to other data type
+a="122"
+a=int(a)
+print(type(a))
+print(a+2)
