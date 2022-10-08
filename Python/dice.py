8597a7be97228271602eb76943df55c4ca5ea590
Sharan Panwar <91478852+SharanPanwar@users.noreply.github.com>
2022-10-08 21:10:54 +0530

    Create dice.py

---
+++ Python/dice.py
@@ -0,0 +1,44 @@
+import random
+x = "y"
+  
+while x == "y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 2:
+        print("[-----]")
+        print("[ 0   ]")
+        print("[     ]")
+        print("[   0 ]")
+        print("[-----]")
+    if no == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[-----]")
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 6:
+        print("[-----]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[-----]")
+         
+    x=input("press y to roll again and n to exit:")
+    print("\n")
