e6e0f48cf4cc18a9f585fc05749973e8a4e7adcf
Jay Patel <64847529+mr-jay-250@users.noreply.github.com>
2022-10-10 11:56:08 +0530

    Create ReverseAString.cpp
    Please merge this program with hacktoberfest-accepted label.

---
+++ CPP/ReverseAString.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  //Your code here
+  int n = str.length();
+  for(int i=n-1; i>=0; --i) {
+     cout << str[i];
+  }
+}
+
