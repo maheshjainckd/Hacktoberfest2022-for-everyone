a3868b0970354f810e060f9a434a576488d98f8d
vighneshdeepweb <vighneshdeepsharan2002@gmail.com>
2022-10-11 17:38:58 +0530

    Added a new Python Script

---
+++ Python/Wifi-Pass/wifi.py
@@ -0,0 +1,14 @@
+#script to grab all the keys i.e.password to the previous connected WIFI/SSID's to your present device.
+
+
+import subprocess
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        print ("{:<30}|  {:<}".format(i, results[0]))
+    except IndexError:
+        print ("{:<30}|  {:<}".format(i, ""))
