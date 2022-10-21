d0ebb3449ada0d142f30b46f589d4dbe4772ca2e
AcqmalFadhilla <acqmalfadhillalatief@gmail.com>
2022-10-21 09:49:42 +0800

    guess the numbers

--- Python/ guess the numbers.py
+++ Python/ guess the numbers.py
@@ -15,4 +15,4 @@ for chances in range(chance):
   elif number > bil:
     print("a little more your prediction is right, the number you enter is big")
 else:
-  print(f"sorry,all your guesses wrong and answer {bil}")
+  print(f"sorry,all your guesses wrong and answer {bil},thanks")
