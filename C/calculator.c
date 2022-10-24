1900a00be522fa01f796cad6bbb7e8250ff8606f
Ankit Mukherjee <mukherjeeankit370@gmail.com>
2022-10-24 10:27:03 +0530

    made changes in the calculator it will now also perform power of number

--- C/calculator.c
+++ C/calculator.c
@@ -5,7 +5,8 @@ int main()
 {
   char oper;
   double first, second;
-  printf("Enter an operator (+, -, *, /): ");
+  double a = 1;
+  printf("Enter an operator (+, -, *, /, ^): ");
   scanf("%c", &oper);
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
@@ -22,6 +23,12 @@ int main()
       break;
     case '/':
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    case'^':
+      for(int j =0; j<second; j++){
+        a = first * a;
+      }
+      printf("%.1lf ^ %.1lf = %.1lf",first,second,a);
       break;
     default:
       printf("Error! operator is not correct");
