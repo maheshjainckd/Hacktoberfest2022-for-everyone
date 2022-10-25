8d3f595e742a3f7e3fed43245515fe688befb58d
Dhanumaalaian <46643072+dhanu-r@users.noreply.github.com>
2022-10-25 16:00:36 +0530

    Update BubbleSort.c

--- C/BubbleSort.c
+++ C/BubbleSort.c
@@ -5,8 +5,8 @@ int main()
 {
   int array[100], n, c, d, swap;
 
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
+  printf("Enter number of elements\n"); 
+  scanf("%d", &n); //integer call
 
   printf("Enter %d integers\n", n);
 
