b62728436172a041526083f870b16e06d9456df4
Harsh Gupta <harshgupta204016@gmail.com>
2022-10-31 20:54:33 +0530

    Create Binary_Search.py

---
+++ Python/Binary_Search.py
@@ -0,0 +1,13 @@
+#function for binary search
+def search(self, nums: List[int], target: int) -> int:
+    left=0
+    right=len(nums)-1
+    while(left<= right):
+        mid=(left+right)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid] < target:
+             left=mid+1
+        else:
+            right=mid-1
+    return -1
